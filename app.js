7375cdb3069593d7d3b5191c3b68866b62a9dcb1
Daniel Soares de Albuquerque <77938356+warehousemapp@users.noreply.github.com>
2021-04-29 12:50:58 -0300

    teste

--- app.js
+++ app.js
@@ -9,6 +9,38 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/search", async (req, res) => {
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = "1jVDqLQw3-3mQ4BB59cE7_kW07qInQsjQVCekdeNPa8A";
+
+  // Get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+  // Read rows from spreadsheet
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Sheet1!A:A",
+  });
+
+console.log(getRows.data)
+  res.send(getRows.data);
+});
+
 app.post("/", async (req, res) => {
   const { request, name } = req.body;
 
@@ -23,7 +55,7 @@ app.post("/", async (req, res) => {
   // Instance of Google Sheets API
   const googleSheets = google.sheets({ version: "v4", auth: client });
 
-  const spreadsheetId = "1J5OesnSTJCgLTTA0hQ_QSk_UPVK1nwRTEkvvRHHrEqM";
+  const spreadsheetId = "1jVDqLQw3-3mQ4BB59cE7_kW07qInQsjQVCekdeNPa8A";
 
   // Get metadata about spreadsheet
   const metaData = await googleSheets.spreadsheets.get({
@@ -49,7 +81,7 @@ app.post("/", async (req, res) => {
     },
   });
 
-  res.send("Successfully submitted! Thank you!");
+  res.json("Successfully submitted! Thank you!");
 });
 
 app.listen(1337, (req, res) => console.log("running on 1337"));
